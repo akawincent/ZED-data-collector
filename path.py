3c76dc966b37785d133c89c05e5cf7da0b5a3e6e
akawincent <3511606256@qq.com>
2023-06-23 20:58:09 +0800

    update file manegenment

---
+++ path.py
@@ -0,0 +1,4 @@
+gt_file_path = ".\\data\\groundtruth.tum"
+times_file_path = ".\\data\\times.txt"
+image0_file_path = ".\\data\\images\\image_0\\"
+image1_file_path = ".\\data\\images\\image_1\\"
--- record.py
+++ record.py
@@ -1,14 +1,13 @@
 import os
 import cv2
-
+import path
 class Recorder:
-    
     # Write data in file
     def record_pose_data( data ):
         timestamp = data[0]
         [tx,ty,tz] = data[1:4:1]
         [qx,qy,qz,qw] = data[4:8:1]
-        with open('groundtruth.tum','a') as file_handle:
+        with open( path.gt_file_path ,'a') as file_handle:
             file_handle.write("{0} {1} {2} {3} {4} {5} {6} {7}\n".format(timestamp,
                                                                      tx,
                                                                      ty,
@@ -20,7 +19,7 @@ class Recorder:
             file_handle.close()
             
     def record_timestamp(timestamp):
-        with open('times.txt','a') as file_handle:
+        with open( path.times_file_path ,'a') as file_handle:
             timestamp = str(round(timestamp * 1e-6,6))
             if(timestamp.__len__() == 16):
                 timestamp = timestamp + str('0')
@@ -31,7 +30,7 @@ class Recorder:
     def record_img_data( LeftImg , RightImg , Timestamp ):
         left_view = LeftImg
         right_view = RightImg
-        left_img_save_path = os.path.join("./images/image_0","{0}.jpg".format(Timestamp))
-        right_img_save_path = os.path.join("./images/image_1","{0}.jpg".format(Timestamp))
+        left_img_save_path = os.path.join( path.image0_file_path ,"{0}.jpg".format(Timestamp))
+        right_img_save_path = os.path.join( path.image1_file_path ,"{0}.jpg".format(Timestamp))
         cv2.imwrite( left_img_save_path , left_view)
         cv2.imwrite( right_img_save_path , right_view)
