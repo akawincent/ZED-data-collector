e82e9b71b74f3fe9fcf3e473f1cc7dbd7ba32300
akawincent <3511606256@qq.com>
2023-06-26 21:28:25 +0800

    file management

--- SVO/export.py
+++ SVO/export.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import cv2
+import path
 import numpy as np
 import pyzed.sl as sl
 
@@ -9,3 +10,12 @@ def progress_bar(percent_done, bar_length=50):
     bar = '=' * done_length + '-' * (bar_length - done_length)
     sys.stdout.write('[%s] %f%s\r' % (bar, percent_done, '%'))
     sys.stdout.flush()
+    
+if __name__ == "__main__":
+    ## Clear file generated before
+    if( os.path.isfile(path.times_file_path)):
+        os.remove(path.times_file_path)
+        print("Times file Deleted successfully")
+    else:
+        print("Times file does not exist")
+    
