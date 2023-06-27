6c3e90e08ac340c8de799f9ec15c5c90f5382615
akawincent <3511606256@qq.com>
2023-06-27 22:57:23 +0800

    command check

--- SVO/export.py
+++ SVO/export.py
@@ -2,6 +2,7 @@ import sys
 import os
 import cv2
 import path
+import glob
 import numpy as np
 import pyzed.sl as sl
 
@@ -18,4 +19,18 @@ if __name__ == "__main__":
         print("Times file Deleted successfully")
     else:
         print("Times file does not exist")
+    for image_file in glob.glob( path.image0_file_path + '*'):
+        if(image_file.__len__() != 0):
+            os.remove(image_file)
+            print("delete"+str(image_file))
+        else:
+            print("image_0 is Already empty")
+    for image_file in glob.glob(path.image0_file_path + '*'):
+        if(image_file.__len__() != 0):
+            os.remove(image_file)
+            print("delete"+str(image_file))
+        else:
+            print("image_1 is Already empty")
+            
+    ## Get command parameters
     
