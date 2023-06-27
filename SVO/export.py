4345076fe516503e141f7e500eede9f20eab77de
akawincent <3511606256@qq.com>
2023-06-27 23:20:49 +0800

    init parameter

--- SVO/export.py
+++ SVO/export.py
@@ -5,6 +5,7 @@ import path
 import glob
 import numpy as np
 import pyzed.sl as sl
+from pathlib import Path
 
 def progress_bar(percent_done, bar_length=50):
     done_length = int(bar_length * percent_done / 100)
@@ -32,5 +33,23 @@ if __name__ == "__main__":
         else:
             print("image_1 is Already empty")
             
-    ## Get command parameters
+    ## Setting SVO parameter
+    svo_file_path = Path(sys.argv[1])
+    camera_params = sl.InitParameters(
+        svo_real_time_mode = False,  # Don't convert in realtime
+        coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Z_UP_X_FWD,
+        coordinate_units = sl.UNIT.METER,
+        depth_mode = sl.DEPTH_MODE.NONE,
+    )
+    camera_params.set_from_svo_file(str(svo_file_path))
+
+    ## Open camera
+    zed = sl.Camera()
+    camera_status = zed.open(camera_params)
+    if camera_status != sl.ERROR_CODE.SUCCESS:
+        print("Fail to open camera offline!")
+        exit(1)
+    print("Camera start to work offline!") 
+    
+    ## 
     
